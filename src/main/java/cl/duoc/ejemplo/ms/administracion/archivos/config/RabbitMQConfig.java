b618be94d21b79c5ab876473bd3a45890f6364d3
Franco Castro <franco.castro.villanueva.90@gmail.com>
2025-07-21 02:33:35 -0400

    Agregar configuración de RabbitTemplate en RabbitMQConfig para mejorar la gestión de mensajes.

--- src/main/java/cl/duoc/ejemplo/ms/administracion/archivos/config/RabbitMQConfig.java
+++ src/main/java/cl/duoc/ejemplo/ms/administracion/archivos/config/RabbitMQConfig.java
@@ -8,6 +8,7 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -42,6 +43,13 @@ public class RabbitMQConfig {
 	Jackson2JsonMessageConverter messageConverter() {
 
 		return new Jackson2JsonMessageConverter();
+	}
+
+	@Bean
+	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter messageConverter) {
+		RabbitTemplate template = new RabbitTemplate(connectionFactory);
+		template.setMessageConverter(messageConverter);
+		return template;
 	}
 
 	@Bean
@@ -97,7 +105,6 @@ public class RabbitMQConfig {
 
 	@Bean
 	Binding dlxBinding() {
-
 		return BindingBuilder.bind(dlxQueue()).to(dlxExchange()).with(DLX_ROUTING_KEY);
 	}
 }
